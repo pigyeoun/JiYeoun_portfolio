14023085a6ce59cf5c62ec068fcce1f0eac95583
pigyeoun <48500680+pigyeoun@users.noreply.github.com>
2019-07-04 18:49:55 +0900

    Add files via upload

---
+++ js/portfolio_2_gif1.js
@@ -0,0 +1,122 @@
+$(document).ready(function() {
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif1").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/iW8Nf.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif1").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/jvkVf.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif2").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/SDxSz.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif2").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/wbSvJ.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif3").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/eTmNg.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif3").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/PpPyL.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif4").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/WL8r9.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif4").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/9xe0C.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif5").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/UmP5t.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif5").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/SHfkL.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif6").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/gSRMd.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif6").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/oDwOX.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif7").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/0JExz.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif7").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/DYiNK.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif8").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/9gLOb.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif8").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/n0M7u.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif9").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/yV4wz.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif9").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/lLK20.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif10").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/5VTMx.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif10").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/l1xpZ.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif11").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/Acs2N.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif11").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/V8PHg.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif12").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/4mLzq.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif12").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/pXt8r.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif13").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/Psu0a.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif13").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/u0N31.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif14").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/sP845.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif14").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/dX4Rj.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif15").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/Jl8js.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif15").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/Fw6EW.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif16").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/ivK8X.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif16").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/Syk8o.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif17").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/OiSF7.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif17").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/qTm6r.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif18").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/lpcs6.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif18").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/tkfMi.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif19").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/UV992.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif19").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/w7i0Y.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif20").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/kcmSz.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif20").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/9dM3U.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif21").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/zg696.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif21").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/VaoAP.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif22").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/ZL82a.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif22").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/TBrgh.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif23").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/lpDNF.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif23").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/HBQ1z.png");});
+
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif24").mouseover(function() {
+    $(this).attr("src","https://ifh.cc/g/VEuav.gif");});
+  $(".new_window_text_wrap .new_window_img .ps1_5_gif24").mouseleave(function() {
+    $(this).attr("src","https://ifh.cc/g/5wlm2.png");});
+});
